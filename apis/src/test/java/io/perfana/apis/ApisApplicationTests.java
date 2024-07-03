05582f67de57ae9c2cee0453e05d9facf2d390a4
Maulik Dave <mdave.5191@gmail.com>
2024-07-03 21:55:19 +0530

    init commit

---
+++ apis/src/test/java/io/perfana/apis/ApisApplicationTests.java
@@ -0,0 +1,13 @@
+package io.perfana.apis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
